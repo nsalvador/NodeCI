3e817ecc20bc27137ee98e3d0ff02b6b72218865
Nigel Salvador <nigelsalvador@gmail.com>
2021-03-19 10:50:55 -0400

    Travis config

--- config/dev.js
+++ config/dev.js
@@ -2,6 +2,8 @@ module.exports = {
   googleClientID:
     '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
   googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
+  mongoURI:
+    'mongodb://nsalvador:Fcm6DU1SY2mZMHkA@cluster0-shard-00-00.nzbcr.mongodb.net:27017,cluster0-shard-00-01.nzbcr.mongodb.net:27017,cluster0-shard-00-02.nzbcr.mongodb.net:27017/blog_dev?ssl=true&replicaSet=atlas-9y1a4c-shard-0&authSource=admin&retryWrites=true&w=majority',
   cookieKey: '123123123',
+  redisUrl: 'redis://127.0.0.1:6379',
 };
